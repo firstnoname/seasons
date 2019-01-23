24ca5a390218863e9eabc2c7ba54373a2cfe6afd
iFirst <first.cmu.se@gmail.com>
2019-01-23 09:42:10 +0700

    This is React tutorial about Components.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+  state = { lat: null, errorMessage: "" };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  // Component Lifecycle.
+  componentDidMount() {
+    // console.log("My component was rendered to the screen");
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        // We called setState!!!
+        this.setState({ lat: position.coords.latitude });
+
+        // We did not!!!
+        // this.state.lat = position.coords.latitude;
+      },
+      err => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+
+  // componentDidUpdate() {
+  //   console.log("My component was just updated - it rerendered");
+  // }
+
+  // React says we have to define render!!
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+
+    return <div>Loading...!</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
